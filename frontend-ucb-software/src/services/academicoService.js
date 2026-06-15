368522229ad8b0d0527e243a3e646b50d1ce7d4b
Ariany Lopez <arianylopez341@gmail.com>
2026-06-15 16:35:21 -0400

    Organizando estructura de repo

--- frontend-ucb-software/src/services/academicoService.js
+++
@@ -1,9 +0,0 @@
-import { fetchAPI } from './api.js';
-
-export const getMallaCurricular = async () => {
-  return await fetchAPI('/Academico/malla');
-};
-
-export const getDetalleAsignatura = async (code) => {
-  return await fetchAPI(`/Academico/asignatura/${code}`);
-};
